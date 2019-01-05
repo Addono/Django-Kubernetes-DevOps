b7cc3c7e1ee7252258ea4d8b8256d3c0d49d91ac
Adriaan Knapen <hi@aknapen.nl>
2019-01-05 11:52:54 +0100

    fix: gunicorn wsgi application start file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Prepare log files and start outputting logs to stdout
+mkdir ./logs
 touch ./logs/gunicorn.log
 touch ./logs/gunicorn-access.log
 tail -n 0 -f ./logs/gunicorn*.log &
 
 export DJANGO_SETTINGS_MODULE=projectx.settings
 
-exec gunicorn projectx.wsgi:application \
+exec gunicorn http2.wsgi:application \
     --name projectx_django \
     --bind 0.0.0.0:5000 \
     --workers 5 \
