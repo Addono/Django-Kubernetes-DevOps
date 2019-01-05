be5f01b116bac44320778f9633906e741196cbe3
Adriaan Knapen <hi@aknapen.nl>
2019-01-05 14:13:04 +0100

    new: attempt to use postgres

--- http2/settings.py
+++ http2/settings.py
@@ -76,13 +76,24 @@ WSGI_APPLICATION = 'http2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if (os.environ.get('POSTGRES_ENABLED')):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'user'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'testing-password'),
+            'HOST': os.getenv('DATABASE_URL', 'localhost'),
+            'PORT': '',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
