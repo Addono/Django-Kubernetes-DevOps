765f559a16d00a461cd9399b38e81409855cc050
Adriaan Knapen <hi@aknapen.nl>
2019-01-05 13:35:48 +0100

    new: move migrate to gitlab-ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,6 @@ touch ./logs/gunicorn.log
 touch ./logs/gunicorn-access.log
 tail -n 0 -f ./logs/gunicorn*.log &
 
-exec python manage.py migrate &
-
 export DJANGO_SETTINGS_MODULE=http2.settings
 
 exec gunicorn http2.wsgi:application \
